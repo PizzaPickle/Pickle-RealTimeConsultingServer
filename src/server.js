474b71e35936a904eee5c8dd46b7a4f42a8595af
sooyeon-kr <sooyeon7027@naver.com>
2024-08-28 11:44:19 +0900

    feat: pug를 사용하여 테스트할 home.pug 작성

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-console.log("hello");
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+
+console.log('hello');
 app.listen(3000);
