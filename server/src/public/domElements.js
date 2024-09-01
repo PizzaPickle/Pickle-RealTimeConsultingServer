90a060ad9b74f2b499bd203c1637edbd6b532260
SooyeonOh <sooyeon7027@naver.com>
2024-09-01 15:17:53 +0900

    delete: 잘못올린 파일 삭제

--- server/src/public/domElements.js
+++
@@ -1,9 +0,0 @@
-// domElements.js
-export const confirmButton = document.getElementById('confirmButton');
-export const toggleScreenShareButton = document.getElementById('toggleScreenShareButton');
-export const toggleMicButton = document.getElementById('toggleMicButton');
-export const toggleVideoButton = document.getElementById('toggleVideoButton');
-export const leaveRoomButton = document.getElementById('leaveRoomButton');
-export const sharedScreen = document.getElementById('sharedScreen');
-export const myFace = document.getElementById('myFace');
-export const peerFace = document.getElementById('peerFace');
