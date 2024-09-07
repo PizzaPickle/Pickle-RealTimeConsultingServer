257803dcf4786d71b156390b21f10e3593d0262d
sooyeon-kr <sooyeon7027@naver.com>
2024-09-08 01:36:10 +0900

    feat: config 파일을 만들어 환경변수 관리를 전역적으로 함

--- src/mq_handler.js
+++ src/mq_handler.js
@@ -1,60 +1,59 @@
-import amqp from 'amqplib';
-import dotenv from 'dotenv';
+import amqp from 'amqplib/callback_api.js';
+import ENV from './config.js';
 import { saveConsultingRoomInfo } from './redis_client.js';
 import { QUEUE_NAMES } from './constants.js';
 
-dotenv.config();
-
 function setupMQ() {
-	const rabbitmqUser = process.env.RABBITMQ_USER;
-	const rabbitmqPassword = process.env.RABBITMQ_PASSWORD;
-	const rabbitmqHost = process.env.RABBITMQ_HOST;
-	const rabbitmqPort = process.env.RABBITMQ_PORT;
-	amqp.connect(`amqp://${rabbitmqUser}:${rabbitmqPassword}@${rabbitmqHost}:${rabbitmqPort}`, (error0, connection) => {
-		if (error0) {
-			throw error0;
-		}
-		console.log(`rabbitMQ(${rabbitmqHost})에 연결 완료`);
-		connection.createChannel((error1, channel) => {
-			if (error1) {
-				throw error1;
-			}
+  amqp.connect(
+    `amqp://${ENV.RABBITMQ_USER}:${ENV.RABBITMQ_PASSWORD}@${ENV.RABBITMQ_HOST}:${ENV.RABBITMQ_PORT}`,
+    (error0, connection) => {
+      if (error0) {
+        throw error0;
+      }
+      console.log(`rabbitMQ(${ENV.RABBITMQ_HOST})에 연결 완료`);
+      connection.createChannel((error1, channel) => {
+        if (error1) {
+          throw error1;
+        }
 
-			const queues = [QUEUE_NAMES.CONSULTING_ROOM_CREATION];
+        const queues = [QUEUE_NAMES.CONSULTING_ROOM_CREATION];
 
-			queues.forEach((queue) => {
-				// channel.deleteQueue(queue);
-				channel.assertQueue(queue, {
-					durable: true,
-				});
+        queues.forEach(queue => {
+          // channel.deleteQueue(queue);
+          channel.assertQueue(queue, {
+            durable: true,
+          });
 
-				console.log(`[*] Queue(${queue})에서 메시지를 기다리고 있습니다. 종료하려면 CTRL+C를 누르세요.`);
+          console.log(
+            `[*] Queue(${queue})에서 메시지를 기다리고 있습니다. 종료하려면 CTRL+C를 누르세요.`
+          );
 
-				channel.consume(
-					queue,
-					async (message) => {
-						if (message != null) {
-							try {
-								const messageContent = JSON.parse(message.content.toString());
-								console.log(` [x] Received at Queue(${queue})`);
-								console.log(messageContent);
+          channel.consume(
+            queue,
+            async message => {
+              if (message != null) {
+                try {
+                  const messageContent = JSON.parse(message.content.toString());
+                  console.log(` [x] Received at Queue(${queue})`);
+                  console.log(messageContent);
 
-								if (queue === QUEUE_NAMES.CONSULTING_ROOM_CREATION) {
-									const { roomId } = messageContent;
-									await saveConsultingRoomInfo({ roomId, roomInfo: messageContent });
-									channel.ack(message);
-								}
-							} catch (error) {
-								console.error('메시지 처리 중 오류 발생: ', error);
-								// channel.nack(message, false, true);
-							}
-						}
-					},
-					{ noAck: false }
-				);
-			});
-		});
-	});
+                  if (queue === QUEUE_NAMES.CONSULTING_ROOM_CREATION) {
+                    const { roomId } = messageContent;
+                    await saveConsultingRoomInfo({ roomId, roomInfo: messageContent });
+                    channel.ack(message);
+                  }
+                } catch (error) {
+                  console.error('메시지 처리 중 오류 발생: ', error);
+                  // channel.nack(message, false, true);
+                }
+              }
+            },
+            { noAck: false }
+          );
+        });
+      });
+    }
+  );
 }
 
 export default setupMQ;
--- src/server.js
+++ src/server.js
@@ -3,13 +3,11 @@ import http from 'http';
 import socketHandler from './socket_handler.js';
 import setupMQ from './mq_handler.js';
 import { Server } from 'socket.io';
-import dotenv from 'dotenv';
+import ENV from './config.js';
 import setupRoutes from './routes/index.js';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -23,15 +21,15 @@ app.use('/public', express.static(join(__dirname, 'public')));
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: process.env.REACT_APP_ORIGIN,
-        methods: ['GET', 'POST'],
-    },
+  cors: {
+    origin: ENV.REACT_APP_ORIGIN,
+    methods: ['GET', 'POST'],
+  },
 });
 setupRoutes(app);
 setupMQ();
 socketHandler(io);
 
 server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log('Server is listening on port 3000');
 });
