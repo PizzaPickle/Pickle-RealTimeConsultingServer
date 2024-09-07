cbc9cb2b9a0ee538f86ace4557605dc6dabd1256
SooyeonOh <sooyeon7027@naver.com>
2024-09-07 23:44:14 +0900

    fix:import 문에서 amqplib/callback_api .js 추가

--- src/mq_handler.js
+++ src/mq_handler.js
@@ -1,4 +1,4 @@
-import amqp from 'amqplib/callback_api';
+import amqp from 'amqplib/callback_api.js';
 import dotenv from 'dotenv';
 import { saveConsultingRoomInfo } from './redis_client.js';
 import { QUEUE_NAMES } from './constants.js';
