8c07fd5f82edc977afa26d9d8c0eeb240d9e228c
sooyeon-kr <sooyeon7027@naver.com>
2024-09-08 05:19:12 +0900

    feat: 룸목록 가져오는 API 작성

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,5 @@
 import consultingRoutes from './consultingRoutes.js';
 
 export default function setupRoutes(app) {
-	app.use(consultingRoutes);
-
-	// 기본 라우트
-	app.get('/', (req, res) => res.send('Welcome to the Consulting API'));
+  app.use(consultingRoutes);
 }
