9c0e34c74a7a15ce1ddb44bf066f5cdc94fdc32c
SooyeonOh <sooyeon7027@naver.com>
2024-09-08 00:26:31 +0900

    fix: dotenv.config() 누락된 함수 추가

--- src/redis_client.js
+++ src/redis_client.js
@@ -1,6 +1,8 @@
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const redisClient = createClient({
     port: process.env.REDIS_PORT || 6379,
     host: process.env.REDIS_HOST || '127.0.0.1',
