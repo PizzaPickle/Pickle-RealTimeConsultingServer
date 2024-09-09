b88277ae5191d601c712f7827bce4ab2ed014671
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-09-09 10:15:56 +0900

    feat: url 변경

--- src/routes/consultingRoutes.js
+++ src/routes/consultingRoutes.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import { getConsultingRoom, getRoomList } from '../controllers/consultingController.js'; // .js 확장자 포함
+import express from "express";
+import {
+  getConsultingRoom,
+  getRoomList,
+} from "../controllers/consultingController.js"; // .js 확장자 포함
 
 const router = express.Router();
 
-router.get('/api/consulting-room', getRoomList);
-router.post('/api/consulting-room/:roomId', getConsultingRoom);
+router.get("/consulting-room", getRoomList);
+router.post("/consulting-room/:roomId", getConsultingRoom);
 
 export default router;
