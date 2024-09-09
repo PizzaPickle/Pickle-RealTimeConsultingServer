33868433d8f0c47bc24827ef1d75b812ce4e10d1
sooyeon-kr <sooyeon7027@naver.com>
2024-09-09 13:00:18 +0900

    feat: nignx로 인해 배포시 필요없는 proxy 설정 제거

--- src/routes/consultingRoutes.js
+++ src/routes/consultingRoutes.js
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 import {
-  getConsultingRoom,
-  getRoomList,
-} from "../controllers/consultingController.js"; // .js 확장자 포함
+    getConsultingRoom,
+    getRoomList,
+} from '../controllers/consultingController.js'; // .js 확장자 포함
 
 const router = express.Router();
 
-router.get("/consulting-room", getRoomList);
-router.post("/consulting-room/:roomId", getConsultingRoom);
+router.get('/consulting-room', getRoomList);
+router.post('/consulting-room/:roomId', getConsultingRoom);
 
 export default router;
