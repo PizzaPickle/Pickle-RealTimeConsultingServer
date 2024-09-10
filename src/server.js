6adb20c5f0c9df8e10176513be62b6c17afce8a2
sooyeon-kr <sooyeon7027@naver.com>
2024-09-11 00:15:03 +0900

    feat: wss URL 추가

--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ app.use(
                     'https://cdn.socket.io',
                     "'unsafe-inline'",
                 ], // 추가
-                connectSrc: ["'self'", 'wss:'],
+                connectSrc: ["'self'", 'wss:', process.env.SOCKET_URL],
                 styleSrc: ["'self'", 'https://cdnjs.cloudflare.com'],
                 fontSrc: ["'self'", 'https://cdnjs.cloudflare.com'],
             },
