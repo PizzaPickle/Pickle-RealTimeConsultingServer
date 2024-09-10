2b528a65aaebc3bebf72a41f8df20be610449baf
sooyeon-kr <sooyeon7027@naver.com>
2024-09-11 02:07:48 +0900

    fix:중복된 함수 제거

--- src/controllers/consultingController.js
+++ src/controllers/consultingController.js
@@ -53,22 +53,3 @@ export function getConsultingRoomByPost(req, res) {
         res.status(500).json({ message: '서버 오류가 발생했습니다.' });
     }
 }
-
-export async function getRoomList(req, res) {
-    try {
-        const { userId } = req.query;
-        console.log('Requested User ID:', userId);
-
-        const roomList = await getRoomListFromRedis(userId);
-        console.log('Fetched room list from Redis:', roomList);
-
-        if (!roomList) {
-            return res.status(404).json({ error: 'Room list not found' });
-        }
-
-        res.json(roomList);
-    } catch (error) {
-        console.error('Error fetching room list:', error);
-        res.status(500).json({ error: 'Failed to fetch room list' });
-    }
-}
