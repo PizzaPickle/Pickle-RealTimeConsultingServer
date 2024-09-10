9cfad9ad444e8798c94f70dcc1eab8b593e7e2a0
sooyeon-kr <sooyeon7027@naver.com>
2024-09-10 19:17:29 +0900

    fix: 디버깅을 위한 코드 추가

--- src/controllers/consultingController.js
+++ src/controllers/consultingController.js
@@ -3,6 +3,8 @@ export function getConsultingRoom(req, res) {
     const { roomId } = req.params;
     const { userName, userId } = req.body;
 
+    console.log('디버깅');
+
     if (!roomId || !userName || !userId) {
         return res.status(400).json({ message: '모든 필드가 필요합니다.' });
     }
