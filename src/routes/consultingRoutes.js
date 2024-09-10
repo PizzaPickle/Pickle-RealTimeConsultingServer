9333ecb79ff644c6f9e495d097c80b5fabc5692c
sooyeon-kr <sooyeon7027@naver.com>
2024-09-11 02:08:53 +0900

    fix: 임포트 누락된 부분 추가

--- src/routes/consultingRoutes.js
+++ src/routes/consultingRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     getConsultingRoom,
     getRoomList,
+    getConsultingRoomByPost,
 } from '../controllers/consultingController.js'; // .js 확장자 포함
 
 const router = express.Router();
